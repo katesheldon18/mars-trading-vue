816a947c0215d1af9205d86b67aa28ea3347a0fe
Kate <katshe18@gmail.com>
2024-05-16 09:38:03 +1200

    Fetched 'item' data and populated table rows

--- src/components/TradingTable.vue
+++ src/components/TradingTable.vue
@@ -8,49 +8,60 @@ export default {
   },
   data() {
       return {
-          tableData: [],
+        tradingData: [],
+        itemData: [],
     };
   },
   methods: {
     async fetchData() {
       try {
         const response = await fetch("https://www.blackpepper.co.nz/api/martian");
-        this.tableData = await response.json();
-        console.log(this.tableData);
+        this.tradingData = await response.json();
+        console.log(this.tradingData);
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching trading data:", error);
       }
+      },
+    async fetchItems() {
+        try {
+            const response = await fetch("https://www.blackpepper.co.nz/api/martian/items");
+            this.itemData = await response.json();
+            console.log(this.itemData);
+        } catch (error) {
+            console.error("Error fetching item data:", error);
+        }
     },
     },
     mounted() {
         this.fetchData();
+        this.fetchItems();
   }
 };
 </script>
 
 <template>
-  <div class="container">
-    <Counter :initialCount="0" />
-    <div>{{ tableData }}</div>
-    <table>
-      <div class="traders"></div>
-      <thead>
-        <tr>
-          <td>Hello world heading</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Hello world cells</td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td>Total</td>
-        </tr>
-      </tfoot>
-    </table>
-  </div>
+    <div class="container">
+        <Counter :initialCount="0" />
+        <!-- <div>{{ tradingData }}</div>
+        <div>{{ itemData.name }}</div> -->
+        <!-- <div v-for="item in itemData.data" :key="item.name">{{ item.name }}</div> -->
+        <table>
+            <div class="traders"></div>
+            <thead>
+                <tr>
+                    <td>Hello world heading</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in itemData.data" :key="item.name">{{ item.name }}</tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td>Total</td>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
 </template>
 
 <style scoped>

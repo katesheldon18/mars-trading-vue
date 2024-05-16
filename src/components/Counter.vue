bba7a249d23e5c5c1b246d201542e82b1675b0f8
Kate <katshe18@gmail.com>
2024-05-16 13:26:55 +1200

    Added buttons & dropdowns

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,53 +1,50 @@
 <script>
 export default {
-  name: 'Counter',
+  name: "Counter",
   props: {
     initialCount: {
       type: Number,
-      default: 0
-    }
+      default: 0,
+    },
   },
   data() {
     return {
-      count: this.initialCount
+      count: this.initialCount,
     };
   },
   methods: {
     increment() {
       this.count++;
-      this.$emit('updateCount', this.count);
+      this.$emit("updateCount", this.count);
     },
     decrement() {
       if (this.count > 0) {
         this.count--;
-        this.$emit('updateCount', this.count);
+        this.$emit("updateCount", this.count);
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
 <template>
-    <div class="container">
-      <button @click="decrement">-</button>
-      <span>{{ count }}</span>
-      <button @click="increment">+</button>
-    </div>
+  <div class="container">
+    <button class="change-count" @click="decrement">-</button>
+    <span>{{ count }} / {{ count }}</span>
+    <button class="change-count" @click="increment">+</button>
+  </div>
 </template>
-
 
 <style scoped>
 .container {
   display: flex;
   align-items: center;
 }
-button {
-  width: 30px;
-  height: 30px;
-  margin: 0 5px;
-}
-span {
-  width: 30px;
-  text-align: center;
+.change-count {
+  border: 1px solid #808080;
+  border-radius: 99%;
+  background-color: transparent;
+  cursor: pointer;
+  margin: 8px;
 }
 </style>
--- src/components/TradingTable.vue
+++ src/components/TradingTable.vue
@@ -7,68 +7,123 @@ export default {
     Counter,
   },
   data() {
-      return {
-        tradingData: [],
-        itemData: [],
+    return {
+      tradingData: [],
+      itemData: [],
+      selectedTrader: "",
     };
   },
   methods: {
     async fetchData() {
       try {
-        const response = await fetch("https://www.blackpepper.co.nz/api/martian");
+        const response = await fetch(
+          "https://www.blackpepper.co.nz/api/martian"
+        );
         this.tradingData = await response.json();
         console.log(this.tradingData);
       } catch (error) {
         console.error("Error fetching trading data:", error);
       }
-      },
+    },
     async fetchItems() {
-        try {
-            const response = await fetch("https://www.blackpepper.co.nz/api/martian/items");
-            this.itemData = await response.json();
-            console.log(this.itemData);
-        } catch (error) {
-            console.error("Error fetching item data:", error);
-        }
+      try {
+        const response = await fetch(
+          "https://www.blackpepper.co.nz/api/martian/items"
+        );
+        this.itemData = await response.json();
+        console.log(this.itemData);
+      } catch (error) {
+        console.error("Error fetching item data:", error);
+      }
     },
-    },
-    mounted() {
-        this.fetchData();
-        this.fetchItems();
-  }
+  },
+  mounted() {
+    this.fetchData();
+    this.fetchItems();
+  },
 };
 </script>
 
 <template>
-    <div class="container">
-        <Counter :initialCount="0" />
-        <!-- <div>{{ tradingData }}</div>
-        <div>{{ itemData.name }}</div> -->
-        <!-- <div v-for="item in itemData.data" :key="item.name">{{ item.name }}</div> -->
-        <table>
-            <div class="traders"></div>
-            <thead>
-                <tr>
-                    <td>Hello world heading</td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="item in itemData.data" :key="item.name">{{ item.name }}</tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td>Total</td>
-                </tr>
-            </tfoot>
-        </table>
+  <div class="container">
+    <h1 class="heading">MARS TRADING PLATFORM</h1>
+    <div class="dropdowns">
+      <div></div>
+      <select>
+        <option class="dropdown dropdown1">Select</option>
+        <option v-for="trader in tradingData.data" :key="trader.name">
+          {{ trader.name }}
+        </option>
+      </select>
+      <select>
+        <option class="dropdown dropdown1">Select</option>
+        <option v-for="trader in tradingData.data" :key="trader.name">
+          {{ trader.name }}
+        </option>
+      </select>
     </div>
+    <table>
+      <thead>
+        <td></td>
+        <td v-for="trader in tradingData.data" :key="trader.name">
+          {{ trader.name }}
+        </td>
+      </thead>
+      <tbody>
+        <tr v-for="item in itemData.data" :key="item.name">
+          {{
+            item.name
+          }}
+          <td><Counter /></td>
+          <td><Counter /></td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+        </tr>
+      </tfoot>
+    </table>
+    <div class="table-buttons">
+      <button class="btn btn-reset">RESET</button>
+      <button class="btn btn-trade">TRADE</button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 24px;
+  letter-spacing: 2px;
+}
+.dropdowns {
+  display: flex;
+}
+.dropdown {
+  padding: 12px;
+  margin: 12px;
+}
+.btn {
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 24px;
+  letter-spacing: 2px;
+  text-align: center;
+  border-radius: 48px;
+  padding: 8px 55px;
+  margin: 0px 12px;
+  cursor: pointer;
+}
+.btn-reset {
+  /* border: 1px solid #ffffff; */
+  border: 1px solid black;
+  background-color: transparent;
+}
+.btn-trade {
+  border: 1px solid #7f7f7f;
+  background-color: #7f7f7f;
+  color: white;
 }
 </style>
